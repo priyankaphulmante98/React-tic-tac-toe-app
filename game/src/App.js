e1f226d53998057d7af583f03101a281f5e24cd4
priyanka <priyankaphulmante98@gmail.com>
2022-09-10 11:01:32 +0530

    1

---
+++ game/src/App.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import Grid from "./Components/Grid";
+import './App.css'
+
+export default function App() {
+  const [winner, setWinner] = useState(0);
+
+  return (
+    <div className='App'>
+      <h1>Tic Tac Toe</h1>
+      <Grid setWinner ={setWinner} winner={wineer} />
+      {winner ==1 || winner ==2 ? (
+        <h2>Yeahh! Player {winner} is the winner</h2>
+        ) : (
+             ""
+      )}
+
+      
+    </div>
+  );
+}
